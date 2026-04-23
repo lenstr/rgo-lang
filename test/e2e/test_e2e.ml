fd1f709add635ef90b4c18253d488e7e2c73bec9
lenstr <ilenstr@gmail.com>
2026-04-23 03:48:00 +0300

    test(e2e): use tracked interop-crud.rg fixture for HTTP regression test

--- test/e2e/test_e2e.ml
+++ test/e2e/test_e2e.ml
@@ -244,49 +244,7 @@ let test_import_unknown_member_no_output () =
 
 (* ---- HTTP CRUD runtime regression tests ---- *)
 
-let crud_source =
-  {|
-use net::http;
-
-let mut items: Vec<String> = Vec::new();
-
-fn handle_items(w: http::ResponseWriter, r: http::Request) {
-    let m = r.method;
-    if m == "GET" {
-        let mut response = "[";
-        let mut first = true;
-        for item in items {
-            if !first {
-                response = response + ", ";
-            }
-            response = response + "\"" + item + "\"";
-            first = false;
-        }
-        response = response + "]";
-        w.write_header(200);
-        w.write(response);
-    } else if m == "POST" {
-        let name = r.form_value("name");
-        if name == "" {
-            w.write_header(400);
-            w.write("missing name");
-        } else {
-            items.push(name);
-            w.write_header(201);
-            w.write("created: " + name);
-        }
-    } else {
-        w.write_header(405);
-        w.write("method not allowed");
-    }
-}
-
-fn main() {
-    let mux = http::new_serve_mux();
-    mux.handle_func("/items", handle_items);
-    http::listen_and_serve("127.0.0.1:3111", mux);
-}
-|}
+let crud_source = read_file "../../.factory/runtime/interop-crud.rg"
 
 let http_get url =
   let body_file = Filename.temp_file "rgo_http_" ".body" in
